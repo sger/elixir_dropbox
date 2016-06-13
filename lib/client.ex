408dbcc03bb407977fe7b9d4500dbfdd39b10f56
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-06-13 16:32:50 +0300

    adding configuration

--- lib/client.ex
+++ lib/client.ex
@@ -1,9 +1,16 @@
 defmodule ElixirDropbox.Client do
+	
+	@dropbox_url Application.get_env(:elixir_dropbox, :dropbox_url)
+
 	def start do
 		HTTPoison.start
 	end
 
 	def test_get do
 		HTTPoison.get! "http://httparrot.herokuapp.com/get"
+	end
+
+	def print_url do
+		IO.puts "#{@dropbox_url}"
 	end
 end

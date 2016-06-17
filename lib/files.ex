9345c75e7f76c99fdca403f872afa5fac293be44
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-06-17 17:59:52 +0300

    updates

--- lib/files.ex
+++ lib/files.ex
@@ -4,9 +4,15 @@ defmodule ElixirDropbox.Files do
     body = %{"path" => path}
     result = to_string(Poison.Encoder.encode(body, []))
     ElixirDropbox.post(client, "/files/create_folder", result) 
+  end
+
+  def delete_folder(client, path) do 
+    body = %{"path" => path}
+    result = to_string(Poison.Encoder.encode(body, []))
+    ElixirDropbox.post(client, "/files/delete", result) 
   end  
 
-    def upload(client, path, file, mode \\ "add", autorename \\ true, mute \\ false) do
+  def upload(client, path, file, mode \\ "add", autorename \\ true, mute \\ false) do
     dropbox_headers = %{
       :path => path,
       :mode => mode,
@@ -15,12 +21,11 @@ defmodule ElixirDropbox.Files do
     }
     headers = %{ "Dropbox-API-Arg" => Poison.encode!(dropbox_headers), "Content-Type" => "application/octet-stream" }
     ElixirDropbox.upload_request(client, "files/upload", file, headers)
-   end
+  end
 
-   def get_metadata(client, path, include_media_info \\ false) do
+  def get_metadata(client, path, include_media_info \\ false) do
     body = %{"path" => path, "include_media_info" => include_media_info}
     result = to_string(Poison.Encoder.encode(body, []))
     ElixirDropbox.post(client, "/files/get_metadata", result)
    end
-
 end

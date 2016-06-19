30ba47c48591167fafd03de886417398142679ca
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-06-19 10:44:01 +0300

    refactor

--- lib/elixir_dropbox.ex
+++ lib/elixir_dropbox.ex
@@ -1,6 +1,8 @@
 defmodule ElixirDropbox do
   use HTTPoison.Base
 
+  @type response :: {any}
+
   @base_url Application.get_env(:elixir_dropbox, :base_url)
   @upload_url Application.get_env(:elixir_dropbox, :upload_url)
 
@@ -9,18 +11,27 @@ defmodule ElixirDropbox do
     post_request(client, "#{@base_url}#{url}", body, headers)
   end
 
-  def process_response(%HTTPoison.Response{status_code: 200, body: body}), do: Poison.decode!(body)
-
-  def process_response(%HTTPoison.Response{status_code: status_code, body: body }) do
+  @spec upload_response(HTTPoison.Response.t) :: response
+  def upload_response(%HTTPoison.Response{status_code: 200, body: body}), do: Poison.decode!(body)
+  def upload_response(%HTTPoison.Response{status_code: status_code, body: body }) do
     cond do
     status_code in 400..599 ->
       {:error, {{:http_status, status_code}, JSON.decode(body)}}
     end
   end
 
+   @spec download_response(HTTPoison.Response.t) :: response
+   def download_response(response) do
+    case response do
+      {:ok, %{body: body, headers: headers, status_code: 200}} ->
+        {:ok, %{file: body, headers: get_header(headers, "dropbox_api_result") |> Poison.decode}}
+      _-> response
+    end
+  end
+
   def post_request(client, url, body, headers) do
     headers = Map.merge(headers, headers(client))
-    HTTPoison.post!(url, body, headers) |> process_response
+    HTTPoison.post!(url, body, headers) |> upload_response
   end
 
   def headers(client) do
@@ -44,11 +55,6 @@ defmodule ElixirDropbox do
 
   def download_request(client, url, data, headers) do
     headers = Map.merge(headers, headers(client))
-    response = HTTPoison.post!("#{@upload_url}#{url}", data, headers)
-    case response do
-      {:ok, %{body: body, headers: headers, status_code: 200}} ->
-        {:ok, %{file: body, headers: get_header(headers, "dropbox_api_result") |> Poison.decode}}
-      _-> response
-    end
+    HTTPoison.post!("#{@upload_url}#{url}", data, headers) |> download_response
   end
 end

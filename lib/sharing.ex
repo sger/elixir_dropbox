ae32e58129400f51a5512f0d1287f7b9717f9653
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-06-21 19:00:29 +0300

    updates

--- lib/sharing.ex
+++ lib/sharing.ex
@@ -1,7 +1,7 @@
 defmodule ElixirDropbox.Sharing do
 
-  def create_shared_link(client) do
-    body = %{"path" => "/Dev", "short_url" => true}
+  def create_shared_link(client, path) do
+    body = %{"path" => path, "short_url" => true}
     result = to_string(Poison.Encoder.encode(body, []))
     ElixirDropbox.post(client, "/sharing/create_shared_link", result)
   end

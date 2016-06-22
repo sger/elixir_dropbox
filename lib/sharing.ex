d223e9c8e23fa948117f74dc87ada71fae21d808
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-06-22 16:10:59 +0300

    adding documentation

--- lib/sharing.ex
+++ lib/sharing.ex
@@ -1,5 +1,14 @@
 defmodule ElixirDropbox.Sharing do
+ @doc """
+  Create shared link
 
+  ## Example
+
+    ElixirDropbox.Sharing.create_shared_link client, "/Path"
+
+  More info at: https://www.dropbox.com/developers/documentation/http/documentation#sharing-create_shared_link
+  """  
+  @spec create_shared_link(Client, binary) :: Map
   def create_shared_link(client, path) do
     body = %{"path" => path, "short_url" => true}
     result = to_string(Poison.Encoder.encode(body, []))

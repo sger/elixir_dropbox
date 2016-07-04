5b45ad16c6dece4bef41b8abe76c6a323bce3a59
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-07-04 20:28:21 +0300

    fixes

--- mix.exs
+++ mix.exs
@@ -29,7 +29,6 @@ defmodule ElixirDropbox.Mixfile do
     [
       { :httpoison, "~> 0.8" },
       { :poison, "~> 1.5" },
-      { :exjsx, "~> 3.2" },
       { :inch_ex, "~> 0.5", only: [:dev, :test] },
       { :json, "~> 0.3.0" },
       { :ex_doc, github: "elixir-lang/ex_doc" } 
